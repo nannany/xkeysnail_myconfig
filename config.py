c474d95bf2531ade1f6e2abb6038ae067b9e1e5c
nannany <ymym1990ymym@gmail.com>
2019-08-19 02:25:50 -0700

    ちょっと足した

--- config.py
+++ config.py
@@ -7,16 +7,26 @@ define_multipurpose_modmap({
 })
 
 define_keymap(None, {
+    # vim like上下左右
     K("RAlt-h"): K("LEFT"),
     K("RAlt-j"): K("DOWN"),
     K("RAlt-k"): K("UP"),
     K("RAlt-l"): K("RIGHT"),
+    # ページup&down
     K("RAlt-q"): K("C-PAGE_UP"),
     K("RAlt-w"): K("C-PAGE_DOWN"),
+    # backspace&delete
     K("RAlt-y"): K("BACKSPACE"),
     K("RAlt-o"): K("DELETE"),
+    # ページ戻る、次へ
     K("RAlt-n"): K("M-LEFT"),
     K("RAlt-DOT"): K("M-RIGHT"),
+    # その他よく使う系
+    K("RAlt-a"): K("C-a"),
+    K("RAlt-c"): K("C-INSERT"),
+    K("RAlt-s"): K("C-s"),
+    K("RAlt-v"): K("C-v"),
+    K("RAlt-z"): K("C-z"),
     K("RAlt-SPACE"): K("ENTER"),
     K("RAlt-HENKAN"): K("ESC"),
     K("RAlt-MUHENKAN"): K("ESC"),
